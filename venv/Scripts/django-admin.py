6d1bc4fc1599033a4863cb1eb890279658ccdc47
papanics <onicsbantu@gmail.com>
2020-11-11 23:21:59 +0800

    commit

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\programming_2019\projects\admin_proj\admin_proj\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
