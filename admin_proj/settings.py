9031e5a5196640beef4b6c020bc643b8957c95b2
papanics <onicsbantu@gmail.com>
2020-11-12 00:03:32 +0800

    Update settings.py

--- admin_proj/settings.py
+++ admin_proj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "i+homa9*pwd&=zs3&mmviqmas^p2pei^3v4v3u2j==_d62+7ov"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'isdlogsystem.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'isdlogsystem.herokuapp.com']
 
 
 # Application definition
